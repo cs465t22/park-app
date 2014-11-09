71290468d9ddb42076cbc43d45af4052b6b5a86a
Smit Patel <patel176@illinois.edu>
2014-11-09 15:40:13 -0600

    added background image to login view

---
+++ ParkApp/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ParkApp
+//
+//  Created by Smit Patel on 11/8/14.
+//  Copyright (c) 2014 CS465. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
